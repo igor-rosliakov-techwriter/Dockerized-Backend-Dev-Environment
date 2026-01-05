5b312cdd6f12c03637f7d03aaf3a7e0a0c7e5d11
Igor Rosliakov <roslyakov.1987@internet.ru>
2026-01-06 00:57:01 +0900

    Create queue.py

---
+++ src/api/app/queue.py
@@ -0,0 +1,10 @@
+"""
+queue.py
+
+Placeholder for Redis queue operations.
+
+Planned responsibilities:
+- enqueue_job(task_id)
+- optionally: get_queue_length()
+- use REDIS_QUEUE_NAME from env
+"""
